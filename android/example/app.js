1123e3313dfb7ae612e5c9e4273cda8bd1973e16
Andrew Natoli <AndrewNatoli@AndrewNatoli.com>
2015-12-10 16:03:46 -0500

    Restored the examples

--- android/example/app.js
+++ android/example/app.js
@@ -6,34 +6,49 @@
 
 // open a single window
 var win = Ti.UI.createWindow({
-	backgroundColor:'white'
+	backgroundColor:'white',
+	layout: 'vertical'
 });
+
 var label = Ti.UI.createLabel();
-win.add(label);
-win.open();
+var progressLabel = Ti.UI.createLabel();
+var durationLabel = Ti.UI.createLabel();
+var bufferingLabel = Ti.UI.createLabel();
+var stateLabel = Ti.UI.createLabel();
+var completeLabel = Ti.UI.createLabel({text: 'Not Complete'});
+window.add(label);
+window.add(durationLabel);
+window.add(stateLabel);
+window.add(progressLabel);
+window.add(bufferingLabel);
+window.add(completeLabel);
+window.open();
 
 // TODO: write your module tests here
-var advancedaudioplayer = require('com.kcwdev.audio');
-Ti.API.info("module is => " + advancedaudioplayer);
-
-label.text = advancedaudioplayer.example();
-
-Ti.API.info("module exampleProp is => " + advancedaudioplayer.exampleProp);
-advancedaudioplayer.exampleProp = "This is a test value";
+var advaudioplayer = require('com.kcwdev.audio');
+Ti.API.info("module is => " + advaudioplayer);
 
 if (Ti.Platform.name == "android") {
-	var proxy = advancedaudioplayer.createExample({
-		message: "Creating an example Proxy",
-		backgroundColor: "red",
-		width: 100,
-		height: 100,
-		top: 100,
-		left: 150
-	});
+    var audioUrl = "http://www.stephaniequinn.com/Music/Canon.mp3";
+    //var audioUrl = "http://yp.shoutcast.com/sbin/tunein-station.pls?id=1966819";
+    //var audioUrl = "http://dir.xiph.org/listen/714343/listen.m3u";
+    //var audioUrl = "http://dir.xiph.org/listen/714343/listen.xspf";
+    var audioUrl2 = "";
+	var player = advaudioplayer.createAudioPlayer({url: audioUrl});
 
-	proxy.printMessage("Hello world!");
-	proxy.message = "Hi world!.  It's me again.";
-	proxy.printMessage("Hello world!");
-	win.add(proxy);
+    player.addEventListener('change', function(e) { stateLabel.text = 'State: ' + e.state + ' ' + e.description; });
+    player.addEventListener('progress', function(e) { progressLabel.text = 'Progress: ' + e.progress; });
+    player.addEventListener('complete', function(e) { 
+        completeLabel.text = 'Complete';
+        player.setUrl(audioUrl2);
+        durationLabel.text = 'Duration: ' + player.getDuration();
+        player.seek(45000);
+        player.play();
+    } );
+    
+    player.addEventListener('buffering', function(e) { bufferingLabel.text = 'Buffering Percent:' + e.percent });
+
+	player.play();
+    durationLabel.text = 'Duration: ' + player.getDuration();
+
 }
-
